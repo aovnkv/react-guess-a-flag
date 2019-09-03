4229a862c909a9092d1cd01713a0efd309dcc69b
Andrei Vaniukov <aovnkv@yahoo.com>
2019-09-03 12:08:37 +0300

    Updates

---
+++ precache-manifest.b1e381be0266e22b9c706e264caa16be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b3b45ec341743e5560a21aa547fe8f",
+    "url": "/react-guess-a-flag/index.html"
+  },
+  {
+    "revision": "64f3964b1a0b3e6ac3c9",
+    "url": "/react-guess-a-flag/static/css/main.55e79b66.chunk.css"
+  },
+  {
+    "revision": "e2bd8af130b57a897579",
+    "url": "/react-guess-a-flag/static/js/2.d2172a01.chunk.js"
+  },
+  {
+    "revision": "64f3964b1a0b3e6ac3c9",
+    "url": "/react-guess-a-flag/static/js/main.9ca7ca27.chunk.js"
+  },
+  {
+    "revision": "c62e694f2a37caec4273",
+    "url": "/react-guess-a-flag/static/js/runtime~main.f6533399.js"
+  }
+]);
